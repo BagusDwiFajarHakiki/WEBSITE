34de628a8b0d4a81963ec45c29c82d69087c0647
gueyangabisin <rizaldisini@gmail.com>
2025-08-05 12:49:28 +0700

    balik

--- webdesa/resources/views/layout/app.blade.php
+++ webdesa/resources/views/layout/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Pasiraman- Dashboard</title>
+    <title>Pasiraman - Dashboard</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{asset('templete/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
