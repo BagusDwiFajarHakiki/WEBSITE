f912329d1e30b6fb62556ccb5e56d49278405387
gueyangabisin <rizaldisini@gmail.com>
2025-08-08 22:17:18 +0700

    cln

--- webdesa/resources/views/pages/beranda/video_profil.blade.php
+++ webdesa/resources/views/pages/beranda/video_profil.blade.php
@@ -14,19 +14,10 @@
     {{-- Form Upload Video --}}
     <form action="{{ route('beranda.simpan_video') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <div class="form-group">
-            <label for="name">Judul Video</label>
-            <input type="text" class="form-control" name="name" required>
-        </div>
-
+        
         <div class="form-group">
             <label for="file_path">File Video (mp4, mov, avi, max 20MB)</label>
             <input type="file" class="form-control" name="file_path" accept="video/*" required>
-        </div>
-
-        <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" name="is_main" value="1">
-            <label class="form-check-label" for="is_main">Jadikan utama?</label>
         </div>
 
         <button type="submit" class="btn btn-primary">Upload</button>
