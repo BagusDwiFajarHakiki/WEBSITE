a2d77f0bb7320b23025c1d2578e94f820c5e1cf2
gueyangabisin <rizaldisini@gmail.com>
2025-08-08 20:45:01 +0700

    route

--- webdesa/routes/web.php
+++ webdesa/routes/web.php
@@ -16,13 +16,3 @@ route::resource('/umkm', UMKMController::class);
 Route::put('/umkm/update-status/{umkm}', [UmkmController::class, 'updateStatus'])->name('umkm.update-status');
 
 Route::get('/video_profil', [App\Http\Controllers\BerandaController::class, 'video_profil'])->name('beranda.video_profil');
-
-Route::get('/tambah_video', [App\Http\Controllers\BerandaController::class, 'tambah_video']);
-Route::post('/simpan_video', [App\Http\Controllers\BerandaController::class, 'simpan_video'])->name('beranda.simpan_video');
-Route::get('/tambah_foto', [App\Http\Controllers\BerandaController::class, 'tambah_foto']);
-Route::post('/simpan_foto', [App\Http\Controllers\BerandaController::class, 'simpan_foto'])->name('beranda.simpan_foto');
-Route::get('/tambah_slogan', [App\Http\Controllers\BerandaController::class, 'tambah_slogan']);
-Route::post('/simpan_slogan', [App\Http\Controllers\BerandaController::class, 'simpan_slogan'])->name('beranda.simpan_slogan');
-Route::put('/update_video/{id}', [App\Http\Controllers\BerandaController::class, 'update_video'])->name('beranda.update_video');
-Route::put('/update_foto/{id}', [App\Http\Controllers\BerandaController::class, 'update_foto'])->name('beranda.update_foto');
-Route::put('/update_slogan/{id}', [App\Http\Controllers\BerandaController::class, 'update_slogan'])->name('beranda.update_slogan');
