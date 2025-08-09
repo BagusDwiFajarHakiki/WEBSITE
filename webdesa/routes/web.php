ac56f3b5f809864580e33f86bc43d3e3f99ca70b
gueyangabisin <rizaldisini@gmail.com>
2025-08-09 10:04:09 +0700

    pengumuman

--- webdesa/routes/web.php
+++ webdesa/routes/web.php
@@ -16,6 +16,24 @@ route::get('/', function () {
 // ROUTE UMKM
 route::resource('/umkm', UMKMController::class);
 Route::put('/umkm/update-status/{umkm}', [UmkmController::class, 'updateStatus'])->name('umkm.update-status');
+
 //ROUTE BERANDA
+Route::post('/simpan_video', [BerandaController::class, 'simpan_video'])->name('beranda.simpan_video');
+Route::get('/video_profil', [BerandaController::class, 'video_profil'])->name('video_profil');
+Route::get('/pengumuman', [BerandaController::class, 'pengumuman'])->name('pengumuman');
+Route::get('/pengumuman/tambah', [BerandaController::class, 'tambah_pengumuman'])->name('pengumuman.tambah');
+Route::post('/pengumuman/simpan', [BerandaController::class, 'simpanPengumuman'])->name('simpan_pengumuman');
+
+//ROUTE DATA PROFIL DESA
+Route::resource('/profil', ProfilController::class);
+Route::post('/simpan-profil-desa', [ProfilController::class,'store']);
+Route::get('/ambil-profil-desa', [ProfilController::class,'show']);
+
+//ROUTE PERANGKAT DESA
+
+//ROUTEBERANDA
+Route::get('/video_profil', [App\Http\Controllers\BerandaController::class, 'video_profil'])->name('beranda.video_profil');
+
+Route::get('/tambah_video', [App\Http\Controllers\BerandaController::class, 'tambah_video']);
 Route::post('/simpan_video', [App\Http\Controllers\BerandaController::class, 'simpan_video'])->name('beranda.simpan_video');
 Route::get('/video_profil', [App\Http\Controllers\BerandaController::class, 'video_profil'])->name('video_profil');
