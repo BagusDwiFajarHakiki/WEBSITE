2eb292c7b4e0e6403b2a3ed214237f25d2a33d12
gueyangabisin <rizaldisini@gmail.com>
2025-08-10 22:01:38 +0700

    model list bumdes

--- webdesa/app/Models/bumdes.php
+++ webdesa/app/Models/bumdes.php
@@ -3,10 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+//model untuk profil bumdes
 class bumdes extends Model
 {
     protected $table = 'bumdes';
 
     protected $guarded = ['deskripsi'];
+}
+
+// Model untuk usaha bumdes
+class ListBumdes extends Model
+{
+    use HasFactory;
+
+    protected $table = 'listbumdes';
+    protected $fillable = [
+        'name',
+        'deskripsi',
+        'fotopath',
+    ];
 }
